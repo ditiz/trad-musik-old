b09915f77df8378231c1021c22755f0af48c5170
Mathieu <mathieu.smarzyk@campus-numerique-in-the-alps.com>
2019-01-21 23:54:25 +0100

    when paste put the cusor at the end

--- client/app/creation/createTraduction.jsx
+++ client/app/creation/createTraduction.jsx
@@ -283,11 +283,30 @@ export class CreateTraduction extends Component {
 
                     newDiv.appendChild(textDiv);
                     base.appendChild(newDiv);
+
+                    this.placeCaretAtEnd(newDiv);
                 }
-            );
-            
+            );            
         }
         this.useTextArea(side, other);
+    }
+
+    placeCaretAtEnd(el) {
+        el.focus();
+        if (typeof window.getSelection != "undefined"
+            && typeof document.createRange != "undefined") {
+            var range = document.createRange();
+            range.selectNodeContents(el);
+            range.collapse(false);
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+        } else if (typeof document.body.createTextRange != "undefined") {
+            var textRange = document.body.createTextRange();
+            textRange.moveToElementText(el);
+            textRange.collapse(false);
+            textRange.select();
+        }
     }
 
     loseFocus() {
