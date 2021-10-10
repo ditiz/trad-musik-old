37142710bd2da77054a90ad548b477412e15d90c
Mathieu <mathieu@kinow.com>
2021-10-10 23:13:58 +0200

    feat(server): use .env for credential smtp

--- server/main.js
+++ server/main.js
@@ -1,67 +1,72 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
 
 Meteor.startup(() => {
+  // Setup SMTP
+  let username = process.env.SMTP_USERNAME;
+  let password = process.env.SMTP_PASSWORD;
+  let server = process.env.SMTP_SERVER;
+  let port = process.env.SMTP_PORT;
 
-	// Setup SMTP
-	let username = 'mail.smarzyk';
-	let password = 'jcxwvbPvaAL62A&4o%8#U%@';
-	let server = 'smtp.gmail.com';
-	let port = '465';
+  // Email server adress
+  process.env.MAIL_URL =
+    "smtps://" +
+    encodeURIComponent(username) +
+    ":" +
+    encodeURIComponent(password) +
+    "@" +
+    encodeURIComponent(server) +
+    ":" +
+    port;
 
-	// Email server adress
-	process.env.MAIL_URL = 'smtps://' +
-		encodeURIComponent(username) + ':' +
-		encodeURIComponent(password) + '@' +
-		encodeURIComponent(server) + ':' + port;
+  // Edit email verification
+  customVerificationEmail();
 
-	// Edit email verification
-	customVerificationEmail();
-	
-	// Custom reset mail
+  // Custom reset mail
 
-	
-	// Activate verification email
-	Accounts.config({
-		sendVerificationEmail: true
-	});
+  // Activate verification email
+  Accounts.config({
+    sendVerificationEmail: true,
+  });
 
-	// Custom verify email url
-	Accounts.urls.verifyEmail = function(token) {
-		return Meteor.absoluteUrl("verify-email?token=" + token);
-	};
+  // Custom verify email url
+  Accounts.urls.verifyEmail = function (token) {
+    return Meteor.absoluteUrl("verify-email?token=" + token);
+  };
 
-	// Custom reset email url
-	Accounts.urls.resetPassword = function(token) {
-		return Meteor.absoluteUrl("reset-password/" + token);
-	}
+  // Custom reset email url
+  Accounts.urls.resetPassword = function (token) {
+    return Meteor.absoluteUrl("reset-password/" + token);
+  };
 });
 
+function customVerificationEmail() {
+  Accounts.emailTemplates.verifyEmail.from = () => {
+    return "Traduction-musik";
+  };
 
-function customVerificationEmail () {
-	Accounts.emailTemplates.verifyEmail.from = () => {
-		return 'Traduction-musik';
-	};
+  Accounts.emailTemplates.verifyEmail.subject = (user) => {
+    return "Vérification de votre adresse email";
+  };
 
-	Accounts.emailTemplates.verifyEmail.subject = (user) => {
-		return 'Vérification de votre adresse email';
-	};
-
-	Accounts.emailTemplates.verifyEmail.text = (user, url) => {
-		return 'Cliquer sur le lien suivant pour activé votre compte: ' + url;
-	};
+  Accounts.emailTemplates.verifyEmail.text = (user, url) => {
+    return "Cliquer sur le lien suivant pour activé votre compte: " + url;
+  };
 }
 
-function customVerificationEmail () {
-	Accounts.emailTemplates.resetPassword.from = () => {
-		return 'Traduction-musik';
-	};
+function customVerificationEmail() {
+  Accounts.emailTemplates.resetPassword.from = () => {
+    return "Traduction-musik";
+  };
 
-	Accounts.emailTemplates.resetPassword.subject = (user) => {
-		return 'Réinitialisation de votre mot de passe';
-	};
+  Accounts.emailTemplates.resetPassword.subject = (user) => {
+    return "Réinitialisation de votre mot de passe";
+  };
 
-	Accounts.emailTemplates.resetPassword.text = (user, url) => {
-		return "Cliquer sur le lien suivant pour "
-			+ "réinitialiser le mot de passe de votre votre compte:" + url;
-	};
+  Accounts.emailTemplates.resetPassword.text = (user, url) => {
+    return (
+      "Cliquer sur le lien suivant pour " +
+      "réinitialiser le mot de passe de votre votre compte:" +
+      url
+    );
+  };
 }
